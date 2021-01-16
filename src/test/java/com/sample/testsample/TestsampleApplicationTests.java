9a8bc4f92487cf9fa3d336fa92af8d85cae80d7f
RichterAstell <xparis1721x@gmail.com>
2021-01-16 15:23:28 +0900

    feat: report出力設定を追加

--- src/test/java/com/sample/testsample/TestsampleApplicationTests.java
+++ src/test/java/com/sample/testsample/TestsampleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sample.testsample;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestsampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sample.testsample;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestsampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
