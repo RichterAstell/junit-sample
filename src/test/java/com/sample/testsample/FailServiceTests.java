02fa733bcad08b7b713a7fb25663db8776f99fab
RichterAstell <xparis1721x@gmail.com>
2021-01-16 16:36:11 +0900

    fix

--- src/test/java/com/sample/testsample/FailServiceTests.java
+++ src/test/java/com/sample/testsample/FailServiceTests.java
@@ -1,11 +1,11 @@
-//package com.sample.testsample;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//public class FailServiceTests {
-//    @Test
-//    public void 失敗するテスト() {
-//        Assertions.assertEquals(100, 200);
-//    }
-//}
+package com.sample.testsample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FailServiceTests {
+    @Test
+    public void 失敗するテスト() {
+        Assertions.assertEquals(100, 200);
+    }
+}
